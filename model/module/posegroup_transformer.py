18eaa4965fb04cbfa4d400e78780451f03ff017e
IceDog <2919175619@qq.com>
2023-07-17 21:48:38 -0400

    Add files via upload

---
+++ model/module/posegroup_transformer.py
@@ -0,0 +1,184 @@
+import torch
+import torch.nn as nn
+from functools import partial
+from timm.models.layers import DropPath
+
+
+class Cross_Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.linear_q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.linear_k = nn.Linear(dim, dim, bias=qkv_bias)
+        self.linear_v = nn.Linear(dim, dim, bias=qkv_bias)
+
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x_1, x_2, x_3):
+        B, N, C = x_1.shape
+        q = self.linear_q(x_1).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        k = self.linear_k(x_2).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        v = self.linear_v(x_3).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        
+        return x
+    
+
+class Mlp(nn.Module):
+    def __init__(self,
+                 in_features,
+                 hidden_features=None,
+                 out_features=None,
+                 act_layer=nn.GELU,
+                 drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+    
+class CHI_Block(nn.Module):
+    def __init__(self, dim, num_heads, mlp_hidden_dim, qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.norm_11 = norm_layer(dim)
+        self.norm_12 = norm_layer(dim)
+        self.norm_13 = norm_layer(dim)
+
+        self.norm_21 = norm_layer(dim)
+        self.norm_22 = norm_layer(dim)
+        self.norm_23 = norm_layer(dim)
+
+        self.norm_31 = norm_layer(dim)
+        self.norm_32 = norm_layer(dim)
+        self.norm_33 = norm_layer(dim)
+
+        self.attn_1 = Cross_Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        self.attn_2 = Cross_Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        self.attn_3 = Cross_Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        self.norm = norm_layer(dim * 3)
+        self.mlp = Mlp(in_features=dim * 3, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x_1, x_2, x_3):
+        x_1 = x_1 + self.drop_path(self.attn_1(self.norm_11(x_2), self.norm_12(x_3), self.norm_13(x_1)))    
+        x_2 = x_2 + self.drop_path(self.attn_2(self.norm_21(x_1), self.norm_22(x_3), self.norm_23(x_2)))  
+        x_3 = x_3 + self.drop_path(self.attn_3(self.norm_31(x_1), self.norm_32(x_2), self.norm_33(x_3)))  
+
+        x = torch.cat([x_1, x_2, x_3], dim=2) # input shape: x_1, x_2, x_3 : [B, F, 256], output shape : x : [B, F, 256 * 3]
+        x = x + self.drop_path(self.mlp(self.norm(x)))
+
+        x_1 = x[:, :, :x.shape[2] // 3]
+        x_2 = x[:, :, x.shape[2] // 3: x.shape[2] // 3 * 2]
+        x_3 = x[:, :, x.shape[2] // 3 * 2: x.shape[2]]
+
+        return  x_1, x_2, x_3    
+
+
+class Transformer(nn.Module):
+    def __init__(self,
+                 depth=3,
+                 args_channel = 32,
+                 embed_dim=256,
+                 mlp_hidden_dim=512,
+                 frame_length=81,
+                 h=8,
+                 drop_rate=0.1,
+                 drop_path_rate=0.2,
+                 norm_layer=None
+                 ):
+        super().__init__()
+
+        drop_path_rate = 0.20
+        attn_drop_rate = 0.
+        qkv_bias = True
+        qk_scale = None
+        
+        norm_layer = partial(nn.LayerNorm, eps=1e-6)
+        
+        self.linear_1 = nn.Linear(5 * args_channel, embed_dim, bias=qkv_bias)
+        self.linear_2 = nn.Linear(6 * args_channel, embed_dim, bias=qkv_bias)
+        self.linear_3 = nn.Linear(6 * args_channel, embed_dim, bias=qkv_bias)
+        
+        self.pos_embed_1 = nn.Parameter(torch.zeros(1, frame_length, embed_dim))
+        self.pos_embed_2 = nn.Parameter(torch.zeros(1, frame_length, embed_dim))
+        self.pos_embed_3 = nn.Parameter(torch.zeros(1, frame_length, embed_dim))
+
+        self.pos_drop_1 = nn.Dropout(p=drop_rate)
+        self.pos_drop_2 = nn.Dropout(p=drop_rate)
+        self.pos_drop_3 = nn.Dropout(p=drop_rate)
+        
+        self.linear_out_1 = nn.Linear(embed_dim, 5 * args_channel, bias=qkv_bias)
+        self.linear_out_2 = nn.Linear(embed_dim, 6 * args_channel, bias=qkv_bias)
+        self.linear_out_3 = nn.Linear(embed_dim, 6 * args_channel, bias=qkv_bias)
+
+        # stochastic depth decay rule
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  
+
+        self._CHI_Block = CHI_Block(
+                dim=embed_dim, num_heads=h, mlp_hidden_dim=mlp_hidden_dim, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[depth-1], norm_layer=norm_layer)
+
+        self.norm = norm_layer(args_channel * 17)
+
+    def forward(self, x_1, x_2, x_3):
+        x_1 = self.linear_1(x_1)
+        x_2 = self.linear_2(x_2)
+        x_3 = self.linear_3(x_3)
+        
+        x_1 += self.pos_embed_1 # torso
+        x_2 += self.pos_embed_2 # lArm, rLeg
+        x_3 += self.pos_embed_3 # rArm, lLeg
+
+        x_1 = self.pos_drop_1(x_1)
+        x_2 = self.pos_drop_2(x_2)
+        x_3 = self.pos_drop_3(x_3)
+        
+        x_1, x_2, x_3 = self._CHI_Block(x_1, x_2, x_3) # input shape: x_1, x_2, x_3 : [B, F, 256]
+        
+        x_1 = self.linear_out_1(x_1)
+        x_2 = self.linear_out_2(x_2)
+        x_3 = self.linear_out_3(x_3)
+        
+        x = torch.cat([x_1, x_2, x_3], dim=2)
+        x = self.norm(x)
+        
+        return x
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
